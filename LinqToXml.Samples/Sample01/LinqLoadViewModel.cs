ae7e6bf991b1f15eb401f79b1607487a7ae4304d
seangnewman <seangnewman@msn.com>
2021-05-19 22:39:46 -0700

    Add project files.

---
+++ LinqToXml.Samples/Sample01/LinqLoadViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Linq;
+
+namespace LinqToXml.Samples.Sample01
+{
+  public class LinqLoadViewModel : CommonBase
+  { 
+    #region LoadUsingXDocument Method
+    public void LoadUsingXDocument()
+    {
+      XDocument doc = 
+            XDocument.Load(AppSettings.Instance.CustomersFile);
+    // retuns all elements as the representation of the document
+      ResultText = doc.FirstNode.ToString();
+    }
+    #endregion
+
+    #region LoadUsingXElement Method
+    public void LoadUsingXElement()
+    {
+      XElement doc = 
+            XElement.Load(AppSettings.Instance.CustomersFile);
+    // Only loads the first node of the document
+      ResultText = doc.FirstNode.ToString();
+    }
+    #endregion
+  }
+}
